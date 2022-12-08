4587733589808dc37c76ab1058bf01308c580048
Denys <86122979+Swartalfheim@users.noreply.github.com>
2022-12-08 02:59:34 +0200

    add code

---
+++ M3HW3/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace M3HW3
+{
+    public class Class1
+    {
+        public delegate void ShowHandler(bool a);
+        public int Pow(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
--- M3HW3/Program.cs
+++ M3HW3/Program.cs
@@ -1,10 +1,26 @@
 ﻿namespace M3HW3
 {
-    internal class Program
+    public class Program
     {
+        public delegate bool ResultHandler(int a, int b, int c);
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("This program asks for three numbers, \n" +
+                "after which it determines whether the product of the first two is divisible by the third number,\n" +
+                "and returns true or false if the number is divisible without a remainder or not, respectively\n");
+            Console.Write("enter the first number: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter the second number: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter the third number: ");
+            int result = Convert.ToInt32(Console.ReadLine());
+            Class2 to = new Class2();
+            ResultHandler r = to.Result;
+            Show(r.Invoke(first, second, result));
+        }
+        public static void Show(bool b)
+        {
+            Console.WriteLine(b);
         }
     }
 }
